16263eb3c7885f9103e444b327cae46f472cb831
Emmanuel Aina <emmanuelaina04@meta.com>
2025-06-19 15:13:39 -0700

    This will allow users to add and delete cards from a specific board

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,36 +1,16 @@
-import React, { useState } from "react";
-import axios from "axios";
-
 const Card = ({ card, onDelete }) => {
-  const { title, description, gif, owner } = card;
-  const [votes, setVotes] = useState(card.votes || 0);
+  const { id, title, description, gif } = card;
 
-  const handleUpvote = async () => {
-    try {
-      await axios.patch(
-        `http://localhost:3000/boards/${card.boardId}/cards/${card.card_id}/votes`,
-        {
-          // Send the updated votes count to the backend
-          votes: votes + 1,
-        }
-      );
-
-      setVotes(votes + 1); // Update the local state with the new vote count
-    } catch (error) {
-      console.error("Error upvoting card:", error);
-    }
+  const handleDelete = () => {
+    onDelete(id);
   };
-
   return (
     <div className="card">
       <h3>{title}</h3>
       <p>{description}</p>
       <img src={gif} alt="GIF" />
-      <p>{owner}</p>
-      <button className="upvote-button" onClick={handleUpvote}>
-        Upvote: {votes}
-      </button>
-      <button className="delete-button" onClick={onDelete}>
+      <button className="upvote-button">Upvote</button>
+      <button className="delete-button" onClick={handleDelete}>
         Delete
       </button>
     </div>
