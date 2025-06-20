b83361b6c3dc10c606d3dcd097f86740685fb378
Emmanuel Aina <emmanuelaina04@meta.com>
2025-06-20 12:39:19 -0700

    this will import images from images folder

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,9 @@
+import image from "/src/assets/images/kudoboard_logo.jpg";
 const Header = () => {
   return (
     <div className="header-component">
       <header className="banner">
-        <img src="/src/assets/images/kudoboard_logo.jpg" alt="Kudoboard Logo" />
+        <img src={image} alt="Kudoboard Logo" />
       </header>
     </div>
   );
